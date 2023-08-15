02f82bf2038ce7cb631fd36f0bc800167cb84870
Eilla-7 <eilafadel@gmail.com>
2023-08-15 15:19:14 -0700

    function error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,19 @@
  *
  * Return:0 Successful
  */
-void print_alphabet(void){
-int i = 'a';
-int j = 'z';
-while (i <= j){
-_putchar(i);
-i++;
 
-}
 int main(void)
 {
 print_alphabet();
+_putchar('\n');
 return(0);
+}
+
+void print_alphabet(void){
+char i = 'a';
+while (i <= 'z'){
+_putchar(i);
+i++;
+}
+return;
 }
