d431842c4f5ad9baf15a7c3df5880750bb9caaa4
Eilla-7 <eilafadel@gmail.com>
2023-08-15 17:09:53 -0700

    h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,14 @@
  * Return:0 Successful
  */
 
-void print_alphabet(void){
+void print_alphabet(void)
+{
 char a = 'a';
 char z = 'z';
-while(a <= z)
+while (a <= z)
 {
 _putchar(a);
 a++;
-return;
 }
+return;
 }
