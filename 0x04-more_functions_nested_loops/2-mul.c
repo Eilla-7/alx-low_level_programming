8c1e9f85ec4b9ed5e882798668ee710a1ce2b440
Eilla-7 <eilafadel@gmail.com>
2023-08-17 11:30:34 -0700

    multiplay

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include<ctype.h>
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * mul - is a function to multiplay toe integers
+ * Return: the result of the operation
+ * @a: the first number
+ * @b: the secound number
+ */
+
+int mul(int a, int b)
+{
+int result = a / b;
+return (result);
+}
