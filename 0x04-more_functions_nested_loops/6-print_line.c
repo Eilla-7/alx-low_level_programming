411e0e0ab94dfb4e4e2a65518e8513bde9f77c2e
Eilla-7 <eilafadel@gmail.com>
2023-08-17 14:06:35 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_line - function prints line
+ * Return: have no return value
+ * @n: number of underscore to be printed
+ */
+
+void print_line(int n)
+{
+n = '_';
+_putchar(n);
+printf("\n");
+return;
+}
