bc0aacae02bed72749ccfb7b75ce4598b79cc81e
Eilla-7 <eilafadel@gmail.com>
2023-08-17 11:58:55 -0700

    print the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - a function to print numbers
+ * Return: it doesn't return any value
+ * @void: it doesn't have a parameter
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+return;
+}
