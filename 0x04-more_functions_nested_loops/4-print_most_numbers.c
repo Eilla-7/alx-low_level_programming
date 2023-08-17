34ab568f1380df7d567bc2fd2cf9319657b02883
Eilla-7 <eilafadel@gmail.com>
2023-08-17 12:34:26 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - a function prints numbers between 0 and 9
+ * @void: this function have no parameter
+ * Return: this function doesn't return any value
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+{
+printf("%d", i);
+}
+}
+return;
+}
