b3dccf4236cca0726ba7b7fb532e4bb5be6839ea
Eilla-7 <eilafadel@gmail.com>
2023-08-17 17:26:53 -0700

    diognal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,8 @@ void print_diagonal(int n)
 {
 int i;
 int j;
- if(n > 0){
+if(n > 0)
+{
 for (i = n; i >= 1; i--)
 {
 for (j = n - 1; j >= i; j--)
@@ -20,7 +21,7 @@ printf(" ");
 }
 printf("'\' \n");
 }
- }
+}
 else if (n <= 0)
 printf("\n");
 return;
