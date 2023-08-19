76cf58524304324a61bc78adc637fcca099d6735
Eilla-7 <eilafadel@gmail.com>
2023-08-19 12:47:41 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_number - a function to print a number
+ * Return: has no return value
+ * @n: the number to print
+ */
+
+void print_number(int n)
+{
+_putchar(n);
+return;
+}
