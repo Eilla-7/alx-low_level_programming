431236ccb802c257eaac1728c57e039fe3617d27
Eilla-7 <eilafadel@gmail.com>
2023-08-22 14:17:22 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print the half
+ * @str: the strin
+ * Return: none
+ */
+
+void puts_half(char *str)
+{
+int j;
+int i;
+int l;
+
+for (l = 0; l < '\0'; l++)
+{
+}
+
+if (l % 2 == 0)
+i = l / 2;
+else
+i = (l - 1) / 2;
+for (j = i; j < l; j++)
+{
+printf("%c", str[j]);
+}
+}
