2ecce943323bda5f4b1fa997e5d000e4413b60ed
Eilla-7 <eilafadel@gmail.com>
2023-08-27 19:52:25 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 int digit, mult, result;
 if (n <= 15 && n >= 0)
-{
+
 for (digit = 0; digit <= n; digit++)
 {
 _putchar('0');
@@ -27,6 +27,8 @@ if (result >= 100)
 _putchar((result / 100) + '0');
 _putchar(((result / 10) % 10) + '0');
 }
+else if(result <= 99 && result >= 10)
+{
 _putchar((result / 10) + '0');
 }
 _putchar('\n');
