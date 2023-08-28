4023604eb0afd2f46e31202dfbd7305fc9ab8a47
Eilla-7 <eilafadel@gmail.com>
2023-08-28 02:18:30 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,9 +9,15 @@
 
 void print_number(int n)
 {
-if (n >= 0)
-printf("%d", n);
-else
-printf("%d", -n);
-return;
+unsigned int k = n;
+
+if (n < 0)
+{
+n *= -1;
+k = n;
+_putchar('-');
+}
+k /= 10;
+print_number(k);
+_putchar((unsigned int) n % 10 - '0');
 }
