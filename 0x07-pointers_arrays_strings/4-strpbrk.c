38518568131788cc2edcd74954c1b1748eb3efbd
Eilla-7 <eilafadel@gmail.com>
2023-08-28 16:46:51 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _strpbrk - returns the characters that semilar in the accept string
+ * @s: the first string
+ * @accept: the second string
+ * Return: the string has similar characters
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+
+while (*s != '\0')
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+return (s);
+
+}
+s++;
+}
+return ('\0');
+}
