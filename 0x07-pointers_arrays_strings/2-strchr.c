3b603ef5a30e3f87a060422b53b10008b259a9a9
Eilla-7 <eilafadel@gmail.com>
2023-08-28 15:07:54 -0700

    locate char in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _strchr - function to locate a character in a string
+ * @s: the string to locate in
+ * @c: the character to locate in the string
+ * Return: the string data
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return s;
+s++;
+}
+if (c == '\0')
+return s;
+return NULL;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ int _putchar(char);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_strchr(char *s, char c);
