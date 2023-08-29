3d6118c9e427ab89867199ef51090872340cba52
Eilla-7 <eilafadel@gmail.com>
2023-08-28 18:15:53 -0700

    diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - print a sum of two diagonal
+ * @a: the result
+ * @size: size of the array
+ * Return: none
+ */
+void print_diagsums(int *a, int size)
+{
+int sum1 = 0, sum2 = 0, i;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * size + i];
+}
+for (i = size - 1; i >= 0; i--)
+{
+sum2 += a[i * size + (size - i - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char);
