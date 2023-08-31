6d8bcb4ec9af55ec0d10816ba86f11c533f9ae3a
Eilla-7 <eilafadel@gmail.com>
2023-08-31 05:17:11 -0700

    palindrome

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion -  a function that returns the natural square root of a number
+ * @n: the number
+ * Return: the root square
+ */
+
+int _sqrt_recursion(int n)
+{
+  if (n < 0)
+    return (-1);
+  
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+int is_palindrome(char *s);
+int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
 int factorial(int n);
 void _print_rev_recursion(char *s);
