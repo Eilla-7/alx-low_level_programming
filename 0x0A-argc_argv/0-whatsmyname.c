0c95f6b9c22dd1ae3f72055cb428799a81e7b5c6
Eilla-7 <eilafadel@gmail.com>
2023-09-03 13:09:00 -0700

    the name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - The Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 success
+ */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  if (argc == 1)
-  printf ("%s\n",argv[0]);
-  return 0;
+if (argc == 1)
+printf("%s\n", argv[0]);
+return (0);
 }
