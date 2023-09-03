f1a2d840499cef6f351e1c139ff2d05dd305d8c6
Eilla-7 <eilafadel@gmail.com>
2023-09-03 16:03:03 -0700

    multiply two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,41 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - a function to read a number throgh a string
+ * @s: the string
+ * Return: a number
+ */
+
+int _atoi(char *s)
+{
+int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
+
+
+
 /**
  * main - Entry point
  * @argc: argument counter
@@ -8,13 +44,12 @@
  */
 int main(int argc, char *argv[])
 {
-int i, mul = 1;
+int num1, num2, mul;
 if (argc > 1 && argc < 4)
 {
-for (i = 1; i < argc; i++)
-{
-mul *= atoi(argv[i]);
-}
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+mul = num1 *num2;
 printf("%d\n", mul);
 return (0);
 }
