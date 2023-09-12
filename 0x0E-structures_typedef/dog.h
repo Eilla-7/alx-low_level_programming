397f1c694450650dbd118a6a7d08f8b860250ba0
Eilla-7 <eilafadel@gmail.com>
2023-09-11 17:32:08 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct to define dog details
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:  the owner o
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
