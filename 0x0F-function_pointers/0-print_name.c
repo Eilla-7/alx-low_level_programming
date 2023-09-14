12ef064c12a0ffbeaa49b1138afb3bdfef3656a7
Eilla-7 <eilafadel@gmail.com>
2023-09-13 17:42:51 -0700

    edit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,11 @@
 #include "function_pointers.h"
 /**
- * 
+ * print_name - a function to print a name
+ * @name: the nmae to print
+ * @f: a function
+ * Return: none
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+f(name);
 }
