fcfd995a2b4c3362c59830fb949aa7c1b3cc14ce
Eilla-7 <eilafadel2238@gmail.com>
2023-09-13 19:15:34 -0700

    function on function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include<stdio.h>
-/*
- * array_iterator - a functon excute a function 
+/**
+ * array_iterator - a functon excute a function
  * @array: the array
  * @size: the size of array
  * @action: a function
@@ -10,6 +10,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
