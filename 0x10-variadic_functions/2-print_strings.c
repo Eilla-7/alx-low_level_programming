6bb224621661b0675f0f84a248332f537cc3d840
Eilla-7 <eilafadel2238@gmail.com>
2023-09-14 17:24:32 -0700

    Names

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,6 +25,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i != (n - 1) && (separator != NULL))
 		printf("%s", separator);
+		if (separator == NULL)
+			return;
 		}
 	printf("\n");
 	va_end(args);
