b86f0472f840fb28733134f979a60449d145902f
Eilla-7 <eilafadel2238@gmail.com>
2023-11-09 15:51:35 -0800

    free the list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - a function that frees a list
+ * @head: point to the head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+	{
+		while (head->prev !=NULL)
+			head = head->prev;
+	}
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
