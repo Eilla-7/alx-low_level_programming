bbaf27e7fc12934cb6c725a1f35e02960b38fd83
Eilla-7 <eilafadel2238@gmail.com>
2024-03-14 05:39:13 +0300

    Changes

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,24 +12,22 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, low = 0, high = size - 1, mid;
+	int low = 0, high = size - 1, mid, i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	while (low <= high)
 	{
-		mid  = low + (high - low) / 2;
+		mid = low + (high - low) / 2;
 		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
 			printf("%d", array[i]);
 			if (i < high)
 				printf(", ");
-
 		}
 		printf("\n");
-
 
 		if (array[mid] == value)
 			return (mid);
@@ -38,7 +36,6 @@ int binary_search(int *array, size_t size, int value)
 		else
 			high = mid - 1;
 	}
-
 	printf("Value %d not found in the array\n", value);
 	return (-1);
 }
